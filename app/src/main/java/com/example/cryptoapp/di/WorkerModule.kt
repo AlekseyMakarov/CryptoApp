5d621a770a8774320a4f106f853852eae909e964
Aleksey <leha2546461@rambler.ru>
2024-07-08 15:08:25 +0300

    fix

--- app/src/main/java/com/example/cryptoapp/di/WorkerModule.kt
+++ app/src/main/java/com/example/cryptoapp/di/WorkerModule.kt
@@ -1,10 +1,7 @@
 package com.example.cryptoapp.di
 
-import androidx.lifecycle.ViewModel
-import androidx.work.ListenableWorker
 import com.example.cryptoapp.data.workers.RefreshDataWorker
 import com.example.cryptoapp.data.workers.WorkerChildFactory
-import com.example.cryptoapp.presentation.CoinViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
